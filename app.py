7656d81ff230f1d178e8e76e9bf6f912057d695a
Camille DE NEEF <camille.deneef@gmail.com>
2022-10-29 16:52:49 +0200

    v0.1 downloading .ts files

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ import requests
 
 from utils.find_vod import find_vod_path, find_vod_host
 from utils.m3u8 import has_muted, count_muted_segments
-
-headers = {"User-Agent": "Mozilla/5.0"}
+from utils.download_vod import create_dir, check_file, download_transport_stream
 
 if __name__ == '__main__':
-    TT = "https://twitchtracker.com/aleczandxr/streams/40195889945"
+    TT = "https://twitchtracker.com/lereglement/streams/46232221100"
     vod_path = find_vod_path(TT)
     vod_url = find_vod_host(vod_path)
     print(vod_url)
-    r = requests.get(vod_url, headers=headers)
+    r = requests.get(vod_url)
+    base_url = vod_url.rpartition('/')[0]
     m3u8 = r.text
-    print(m3u8[:100])
+    vod_name = "vod_" + TT.rsplit('/')[-3] + "_" + TT.rsplit('/')[-1] # streamer name + vod ID
+    create_dir(vod_name)
     if has_muted(m3u8):
         print(f'VOD has {count_muted_segments(m3u8)} muted segments')
     else:
         print('VOD has no muted content')
+    
+    # Download muted segments first if aviable, else download in order
+    for record in m3u8.splitlines():
+        if '#' not in record:
+            download_transport_stream(vod_name + '/' + record, base_url + '/' + record)
